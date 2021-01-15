66ae7e9ea1e5e18057e09afe6401d1a1168ea50d
Valentina Vorfolomeeva <vorfolomeevav@gmail.com>
2021-01-15 16:26:35 +0200

    Updates

---
+++ precache-manifest.690f1fb01b569b70d96ce5b7987680d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af5f575474ea2c8364eef311eac8bdd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76cb998313068d8b425c",
+    "url": "./static/css/main.f7ff6e14.chunk.css"
+  },
+  {
+    "revision": "f1d5ac16905ab8c2af6d",
+    "url": "./static/js/2.87d603cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.87d603cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cb998313068d8b425c",
+    "url": "./static/js/main.1e32f7f2.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
